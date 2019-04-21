69a0f1f6c56d5e08f90c94e27b7b66f25b614cec
Celia <celdrake@gmail.com>
2019-04-21 09:28:15 +0200

    4. Matrix using data, and some styles

--- src/components/GameBox.vue
+++ src/components/GameBox.vue
@@ -1,17 +1,60 @@
 <template>
   <div class="game-box">
-    Game box here
+    <div class="game-box__row" v-for="(row, rowIndex) in matrix" :key="rowIndex">
+      <div class="game-box__tile" v-for="(cell, cellIndex) in row" :key="cellIndex">
+        {{ cell.content }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'GameBox.vue',
-  }
+    data() {
+      return {
+        matrix: this.initMatrix(4),
+      };
+    },
+    methods: {
+      initMatrix(gameSize) {
+        const matrix = [];
+        for (let row = 0; row < gameSize; row += 1) {
+          const rowColumns = [];
+          for (let col = 0; col < gameSize; col += 1) {
+            rowColumns.push({
+              display: true,
+              content: Date.now() % 1000,
+            });
+          }
+          matrix.push(rowColumns);
+        }
+        return matrix;
+      },
+    },
+  };
 </script>
 
 <style lang="scss">
   .game-box {
-    border: 1px solid gray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    flex-direction: column;
+    &__row {
+      display: flex;
+      flex-wrap: nowrap;
+      overflow: hidden;
+      transition: all .2s ease-out;
+    }
+    &__tile {
+      padding: 1.5em;
+      background: #3a2a25;
+      color: white;
+      margin: 0;
+      border: 2px solid gray;
+      border-collapse: collapse;
+    }
   }
 </style>
